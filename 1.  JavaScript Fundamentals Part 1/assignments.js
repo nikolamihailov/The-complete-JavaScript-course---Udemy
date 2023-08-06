120387c99fce100432a46771ba73e0d6a614bbbe
nikola mihailov <nikolanm06@abv.bg>
2023-08-06 20:52:09 +0300

    course started

---
+++ 1.  JavaScript Fundamentals Part 1/assignments.js
@@ -0,0 +1,17 @@
+// LECTURE: Values and Variables
+
+const country = "Bulgaria";
+const continent = "Europe";
+const population = 6800000;
+
+console.log(country, continent, population);
+
+// LECTURE: Data Types
+
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
---
+++ 1.  JavaScript Fundamentals Part 1/script.js
@@ -0,0 +1,2 @@
+let js = "amazing";
+if (js === "amazing") alert("Hello!");
