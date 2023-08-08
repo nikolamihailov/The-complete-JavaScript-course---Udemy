4728d4a022c4306f9e7ab0563cc432c6e083c60e
nikola mihailov <nikolanm06@abv.bg>
2023-08-08 18:23:06 +0300

    wrong formula usage fixed

--- 1.  JavaScript Fundamentals Part 1/Coding Challenge #1/challengeOne.js
+++ 1.  JavaScript Fundamentals Part 1/Coding Challenge #1/challengeOne.js
@@ -1,9 +1,9 @@
 const marksWeight = 78;
-const marksHeight = 169;
+const marksHeight = 1.69;
 const marksBMI = marksWeight / (marksHeight ** 2);
 
 const johnsWeight = 92;
-const johnsHeight = 195;
+const johnsHeight = 1.95;
 const johnsBMI = johnsWeight / (johnsHeight ** 2);
 
 const markHigherBMI = marksBMI > johnsBMI;
