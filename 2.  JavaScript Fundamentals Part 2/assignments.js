ffb5481b5fb3bf75bbe760e67fd051a020bad393
nikola mihailov <nikolanm06@abv.bg>
2023-08-10 23:24:52 +0300

    section 2 started

---
+++ 2.  JavaScript Fundamentals Part 2/assignments.js
@@ -0,0 +1,13 @@
+// LECTURE: Functions
+
+function describeCountry(country, population, capital) {
+    return `${country} has ${country} million people and its capital city is ${capital}.`;
+}
+const bulgaria = describeCountry("Bulgaria", 6.8, "Sofia");
+const spain = describeCountry("Spain", 47, "Madrid");
+const italy = describeCountry("Italy", 59, "Rome");
+
+
+console.log(bulgaria);
+console.log(spain);
+console.log(italy);
---
+++ 2.  JavaScript Fundamentals Part 2/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/* VSC shorcuts 
+
+    alt plus (up, down arrow to move line)
+    ctrl plus N to select next occurrence
+    ctrl plus D to select all occurrences of selected
+
+*/
