438c152efa57077d1f0d6c58f0ff35f0e79eb606
nikola mihailov <nikolanm06@abv.bg>
2023-08-21 21:30:47 +0300

    coding challenge one and two added

---
+++ 8. Data Structures, Modern Operators and Strings/Coding Challenge #1/challengeOne.js
@@ -0,0 +1,61 @@
+// predefined data for usage
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+        'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
+
+// solution 
+const [players1, players2] = game.players;
+const [gk, ...fieldPlayers] = players1;
+const allPlayers = [...players1, ...players2];
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+const { odds: { team1, x: draw, team2 } } = game;
+
+const printGoals = function (...players) {
+    console.log(`Total scored goals - ${players.length}:`);
+    players.forEach(p => console.log(p));
+
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
---
+++ 8. Data Structures, Modern Operators and Strings/Coding Challenge #2/challengeTwo.js
@@ -0,0 +1,66 @@
+// predefined data for usage
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+        'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
+
+// solution
+
+for (const [idx, goalScorer] of game.scored.entries()) {
+    console.log(`Goal ${idx + 1}: ${goalScorer}`);
+}
+
+//const avgOdd = Object.values(game.odds).reduce((a, b) => a + b, 0) / Object.values(game.odds).length;
+let sum = 0;
+for (const odd of Object.values(game.odds)) {
+    sum += odd;
+}
+const avgOdd = (sum / Object.values(game.odds).length).toFixed(2);
+console.log(avgOdd);
+
+for (const [team, odd] of Object.entries(game.odds)) {
+    console.log(`Odd of ${team === "x" ? "draw" : `victory ${game[team]}:`} ${odd}`);
+}
+
+const scorer = {};
+for (const goalScorer of game.scored) {
+    scorer[goalScorer] ? scorer[goalScorer]++ : scorer[goalScorer] = 1;
+}
+console.log(scorer);
